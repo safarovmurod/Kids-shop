521c4f7fa4b766cfe1fee995dc7e8210223b7b20
SAFAROV MUROD <genspark_dev@genspark.ai>
2026-08-31 20:05:07 +0000

    chore: clean all pages and components, reset router and layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,10 @@
 import { Outlet } from "react-router"
 import { Box } from "@mui/material"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
 
 export default function Layout() {
   return (
-    <Box sx={{ minHeight: "100vh", backgroundColor: "#1C1C1C" }}>
-      <Header />
+    <Box>
       <Outlet />
-      <Footer />
     </Box>
   )
 }
